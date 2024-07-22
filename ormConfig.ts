e59b109c457dc2388dc11aa9370dba758571db3b
Keshavsaini22 <keshavsainikesu@gmail.com>
2024-07-22 12:50:10 +0530

    migration pending

--- ormConfig.ts
+++ ormConfig.ts
@@ -1,6 +1,11 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
+import { ConfigService } from '@nestjs/config';
+import { SeederOptions } from 'typeorm-extension';
 
-export const ormConfig: TypeOrmModuleOptions = {
+export const dataSourceOptions = (
+    configService: ConfigService,
+): DataSourceOptions & SeederOptions => ({
     type: "postgres",
     username: 'postgres',
     password: 'root',
@@ -9,4 +14,16 @@ export const ormConfig: TypeOrmModuleOptions = {
     port: 5432,
     entities: ["dist/src/domaain/**/*.entity{.ts,.js}"],
     synchronize: false,
-}
+    migrationsTableName: 'custom_migration_table',
+    migrations: ['dist/src/infracture/database/migrations/*{.ts,.js}'],
+})
+
+export const dataSource = new DataSource(dataSourceOptions(new ConfigService()),)
+
+dataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    })
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ormConfig } from 'ormConfig';
+import { dataSourceOptions } from 'ormconfig';
+import { UserModule } from './features/user/user.module';
+import { TodoModule } from './features/todo/todo.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormConfig)],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => dataSourceOptions(configService),
+      inject: [ConfigService],
+    }),
+    UserModule,
+    TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
